359b7ea527745a493e5dc8d9294947185b0f2f5d
kevennfilps <keven.rodrigues.aguiar@gmail.com>
2019-07-19 11:31:50 -0400

    Endpoints/clientes{id}

---
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new org.hibernate.ObjectNotFoundException("Objeto não Encontrado", + id + ", Tipo: " + Cliente.class.getName() ));
+		
+		}
+}
+
+
